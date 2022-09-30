1319d3b9750146db001606c237818b2cf7f9a6a7
Manikprabhu1999 <mbjewargi@gmail.com>
2022-09-30 23:22:15 +0530

    day5

---
+++ single_digit.sh
@@ -0,0 +1,26 @@
+echo "Enter a single digit number"
+read x
+case $x in
+	0)
+	echo "zero";;
+	1)
+	echo "one";;
+	2)
+        echo "Two";;
+	3)
+        echo "Three";;
+	4)
+        echo "Four";;
+	5)
+        echo "Five";;
+	6)
+        echo "six";;
+	7)
+        echo "seven";;
+	8)
+        echo "Eight";;
+	9)
+        echo "Nine";;
+	*)
+	echo "Number is not a single digit number";;
+esac
